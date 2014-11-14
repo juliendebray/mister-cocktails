48907c776ef977d74db9b29df6053e97c6d14dfa
Julien Debray <julien.debray.pro@gmail.com>
2014-11-14 12:23:58 +0100

    images upload

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'better_errors'
 
 gem 'figaro'
 
+gem 'aws-sdk'
+gem 'paperclip'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,9 @@ class Cocktail < ActiveRecord::Base
   has_many :ingredients, through: :consultations
 
   validates :name, presence: true
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :picture,
+      content_type: /\Aimage\/.*\z/
 end

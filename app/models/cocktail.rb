06a6d7a5cd823872a233267c2aff027c2fc15cab
Julien Debray <julien.debray.pro@gmail.com>
2014-11-14 14:40:33 +0100

    ready to beautifulize

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -8,4 +8,6 @@ class Cocktail < ActiveRecord::Base
     styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture,
       content_type: /\Aimage\/.*\z/
+
+  validates :picture, presence: true
 end
